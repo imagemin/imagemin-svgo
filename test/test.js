2520919d7a17e123cfd89728a3a1a70c5267b6e8
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-24 11:19:14 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,23 @@
+/*global describe, it */
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var Imagemin = require('image-min');
+var path = require('path');
+var svgo = require('../');
+
+describe('svgo()', function () {
+    it('should optimize a SVG', function (cb) {
+        var imagemin = new Imagemin();
+
+        imagemin
+            .src(path.join(__dirname, 'fixtures/test.svg'))
+            .use(svgo())
+            .optimize(function (err, file) {
+                assert(file.contents.length < fs.statSync(imagemin.src()).size);
+                assert(file.contents.length > 0);
+                cb();
+            });
+    });
+});
