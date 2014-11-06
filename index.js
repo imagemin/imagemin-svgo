857cc4119c2f242aefd355847ad0a9551797989a
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-06 15:28:08 +0100

    Replace ampersands

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ module.exports = function (opts) {
 
 		try {
 			svgo.optimize(file.contents.toString('utf8'), function (res) {
+				res.data.replace(/&/g, '&amp;');
 				file.contents = new Buffer(res.data);
 				cb(null, file);
 			});
