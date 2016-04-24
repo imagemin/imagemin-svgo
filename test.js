d4302957371043fc84c60a6e3650a321a4b91611
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-24 16:37:07 +0200

    Update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,28 +1,24 @@
 import test from 'ava';
-import imageminSvgo from './';
+import m from './';
 
-test('optimize a SVG', t => {
-	t.plan(1);
-
-	imageminSvgo()('<svg><style> circle {} </style></svg>').then(data => {
-		t.is(data, '<svg><style></style></svg>');
-	});
+test('optimize a SVG', async t => {
+	t.is(await m()('<svg><style> circle {} </style></svg>'), '<svg><style></style></svg>');
 });
 
-test('support SVGO options', t => {
-	t.plan(1);
+test('support SVGO options', async t => {
+	const data = await m({
+		plugins: [{
+			removeStyleElement: true
+		}]
+	})('<svg><style> circle {} </style></svg>');
 
-	imageminSvgo({plugins: [{
-		removeStyleElement: true
-	}]})('<svg><style>  circle {} </style></svg>').then(data => {
-		t.is(data, '<svg/>');
-	});
+	t.is(data, '<svg/>');
 });
 
-test('error on corrupt SVG', t => {
-	t.plan(1);
+test('error on corrupt SVG', async t => {
+	t.throws(m()('<svg>style><</style></svg>'), /Error in parsing SVG/);
+});
 
-	imageminSvgo()('<svg><style> circle {} </style></svg>').catch(err => {
-		t.truthy(err);
-	});
+test('ignore non valid SVG', async t => {
+	t.is(await m()('<html></html>'), '<html></html>');
 });
