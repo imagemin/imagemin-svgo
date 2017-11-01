130da5ac38018e39a2d345b16a7907d4293ec390
Kevin Martensson <kevinmartensson@gmail.com>
2017-11-01 19:39:51 +0100

    Revert `7c2478d` and `f6853b4`

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-const bufferFrom = require('buffer-from');
 const isSvg = require('is-svg');
 const SVGO = require('svgo');
 
@@ -16,6 +15,14 @@ module.exports = opts => buf => {
 
 	const svgo = new SVGO(opts);
 
-	// TODO: Use `Buffer.from` when targeting Node.js >=6
-	return svgo.optimize(buf).then(res => bufferFrom(res.data));
+	return new Promise((resolve, reject) => {
+		svgo.optimize(buf, res => {
+			if (res.error) {
+				reject(new Error(res.error));
+				return;
+			}
+
+			resolve(new Buffer(res.data)); // eslint-disable-line unicorn/no-new-buffer
+		});
+	});
 };
